bd9a8157548b4536c31c5357fe8284150b2210b8
saman keighobadi <saman.keighobadi@gmail.com>
2021-07-14 17:35:59 +0430

    styling Loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Loading = () => {
-    return (
-        <div>
-            <h1>Loading ... </h1>
-        </div>
-    );
+  return (
+    <div className="">
+      <h1 className="lg:text-5xl text-3xl font-bold text-gray-800 text-center mt-32">
+        Loading ...{" "}
+      </h1>
+    </div>
+  );
 };
 
 export default Loading;
