c94d6d43ce8cd60fa6db2a98b666b631f8151e39
saman keighobadi <saman.keighobadi@gmail.com>
2021-07-14 17:36:56 +0430

    Create Image not found compoenent

---
+++ src/components/ImageNotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ImageNotFound = () => {
+  return (
+    <div>
+      <h1 className="lg:text-5xl  sm:text-3xl font-bold text-gray-800 text-center mt-32">
+        Image Not Found!!
+      </h1>
+    </div>
+  );
+};
+
+export default ImageNotFound;
